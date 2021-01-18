e7ab1d03d56ac975e1553cacec576082cf457750
pdrake1988 <pdrake1@me.com>
2021-01-17 19:49:11 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule
